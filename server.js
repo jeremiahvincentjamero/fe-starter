5cac519db0cb012fc0a33d3e01ec19d07d133e89
Jeremiah Vincent Jamero <fncvincentjamero@gmail.com>
2022-02-17 15:32:28 +0800

    Change to dynamic ports

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const express = require("express");
 const server = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+console.log(process.env.PORT)
 server.use(express.static("public"));
 
 server.use("/api/dashboard", function (req, res) {
